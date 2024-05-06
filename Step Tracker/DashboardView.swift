c1c63829c742b2b1a12ed10db67a5f3045be5c0b
James Menkal <gojamesgo85@gmail.com>
2024-05-06 13:02:02 -0600

    hasSeenPermissionPriming AppStorage

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -23,6 +23,8 @@ enum HealthMetricContext: CaseIterable, Identifiable {
 
 struct DashboardView: View {
     
+    @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
+    @State private var isShowingPermissionPrimingSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
     var isSteps: Bool { selectedStat == .steps}
     
@@ -87,10 +89,19 @@ struct DashboardView: View {
                 }
             }
             .padding()
+            .onAppear {
+                isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
+            }
             .navigationTitle("Dashboard")
             .navigationDestination(for: HealthMetricContext.self) { metric in
                 HealthDataListView(metric: metric)
             }
+            .sheet(isPresented: $isShowingPermissionPrimingSheet) {
+                // fetch health data
+            } content: {
+                HealthKitPermissionPrimingView(hasSeen: $hasSeenPermissionPriming)
+            }
+
         }
         .tint(isSteps ? .pink : .indigo)
     }
@@ -98,4 +109,5 @@ struct DashboardView: View {
 
 #Preview {
     DashboardView()
+        .environment(HealthKitManager())
 }
